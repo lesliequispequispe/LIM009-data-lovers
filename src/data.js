691aafa12b8f9dbdc022219e56ce82ab7bda9a97
lesliequispequispe <lesliequispe2605@gmail.com>
2019-03-18 10:55:17 -0500

    quitando los enlaces boots

--- src/data.js
+++ src/data.js
@@ -1,16 +1,4 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-const renderizando = document.getElementById("renderizando");
-console.log(POKEMON.pokemon);
-
-let pokemon1= POKEMON.pokemon; 
-for(let i=0; i<pokemon1.length; i++) {
-console.log(pokemon1[i].name);
-renderizando.innerHTML +='<div class="col-xs-6 col-md-3"><a href="#" class="thumbnail"><img src= ' + pokemon1[i].img + ' alt="Bulbasaur"><div class="caption"><h3>' + pokemon1[i].name + '</h3><p> height:  ' +  pokemon1[i].height +  '</p> <br><p> weight:  ' +  pokemon1[i].weight +  '</p> <br><p> spawn_chance:  ' +  pokemon1[i].spawn_chance +  '</p> <br></div></a> </div>'
-
-}
+/* /* Manejo de data */
 
 const example = () => {
   return 'example';
--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 /* Manejo del DOM */
+const renderizando = document.getElementById("renderizando");
+/* console.log(POKEMON.pokemon); */
+let pokemon1= POKEMON.pokemon; 
+for(let i=0; i<pokemon1.length; i++) {
+/* console.log(pokemon1[i].name); */
+renderizando.innerHTML +='<div class="col-xs-6 col-md-3"><a href="#" class="thumbnail"><img src= ' + pokemon1[i].img + ' alt="Bulbasaur"><div class="caption"><h3>' + pokemon1[i].name + '</h3><p> height:  ' +  pokemon1[i].height +  '</p> <br><p> weight:  ' +  pokemon1[i].weight +  '</p> <br><p> spawn_chance:  ' +  pokemon1[i].spawn_chance +  '</p></p> <br><p> egg:  ' +  pokemon1[i].egg +  '</p> </p> <br><p> avg_spawns:  ' +  pokemon1[i].avg_spawns +  '</p>  <br> </p> <br><p> type:  ' +  pokemon1[i].type +  '</p>  <br><br><p> weaknesses:  ' +  pokemon1[i].weaknesses +  '</p><br><p> multipliers:  ' +  pokemon1[i].multipliers +  '</p> <br><p> next_evolution:  ' +  pokemon1[i].next_evolution + ' </p> </div></div></div></a></div>'
+}
+/* id de menu de opciones */
+let optionMenu = document.getElementById("option-menu");
+optionMenu.addEventListener("change", () => {
+ /* console.log(optionMenu.value);  */
+})
+/* lista desplegalble ordenar de la a z */
+let orderAz = document.getElementById("order-a-z");
+/* lista desplegable de z a */
+let orderZa = document.getElementById("order-z-a");
+/*lista desplegable  filtrar por tipo de pokemon  */
+let pokemonType = document.getElementById("pokemon-type");
+/* lista desplegable para calcular de kg a libras */
+let calculate = document.getElementById("calculate");
