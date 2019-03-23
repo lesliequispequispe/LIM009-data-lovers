a2e8f0332a335ea7dd9b1ac73c030d8470ab9b66
lesliequispequispe <lesliequispe2605@gmail.com>
2019-03-23 17:43:36 -0500

    funcion de ascendente y descendente ordenado terminado

--- src/data.js
+++ src/data.js
@@ -3,8 +3,35 @@
 const example = () => {
   return 'example';
 }; */
-/* pokemon1.getElementById(pokemon1); */
-/*const newArr = [];
-const arrValues = Object.values();
-const arrLength = arrValues.length;
-console.log(arrLength)*/
+/* arrDataPokemons2=POKEMON.pokemon;
+
+const entrandoArrayDeObjetosPokemons = (datos) => {
+  let propiedadesYvaloresPokemons = '';
+  for(let i = 0; i < datos.length; i++){
+propiedadesYvaloresPokemons += pokemon.name;
+console.log(propiedadesYvaloresPokemons);
+
+
+  } */
+
+/* let arrDataPokemons2= document.getElementById("arrDataPokemons2");
+const newArr = [];
+const arrayPropiedadesPokemon= Object.keys(POKEMON);
+const arrayValuesPokemon = Object.values(POKEMON);
+const arrayLengthPokemon = arrayValuesPokemon.length;
+console.log(arrayPropiedadesPokemon); */
+/* console.log(arrayLengthPokemon); */
+/* 
+for(let i = 0; i<arrayValuesPokemon.length;i++){
+if(i===0 ){
+newArr.push(arrayValuesPokemon[i].name);  }
+console.log(newArr);
+} */
+/* const arrayValuesDePokemons = Object.values(POKEMON);
+console.log(arrayValuesDePokemons);
+
+arrayValuesDePokemons.sort(function(a,b) {
+	var x = a.name.toLowerCase();
+	var y = b.name.toLowerCase();
+	return x < y ? -1 : x > y ? 1 : 0;
+}); */
--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@
 const containerPokemonsElement = document.getElementById("renderizando");
 /* POKEMON es un objeto y pokemon es un array de objetos y trae el array de objetos en pokemon1*/
 const arrDataPokemons = POKEMON.pokemon;
-
 /* Creando a la función para pintar los pokemons */
 const pintarPokemons = (data) => {
  let string = '';
  for(let i = 0; i < data.length; i++){
-   string += `<div class="col-xs-6 col-md-3">
+   string += `<div class="ancho">
                <img src=${data[i].img} alt=${data[i].name} />
                <div>
                 <h3>nombre: ${data[i].name} </h3>
@@ -21,11 +20,51 @@ const pintarPokemons = (data) => {
                 <p>multiplicadores:${data[i].multipliers}</p>  
               </div>
               </div>`
-
  }
  containerPokemonsElement.innerHTML = string;
-
 };
 /* llamando a la función para pintar los pokemons */
 pintarPokemons(arrDataPokemons);
+/* evento click para nuestros botones */
+
+const valoresSelect = document.getElementById("valores-select")
+
+const ordenarAz = document.getElementById("ordenarAz");
+ordenarAz.addEventListener("click", () => {
+ /*  console.log(ordenarAz); */
+/*  const arrayValuesDePokemons = Object.values(POKEMON);
+ console.log(arrayValuesDePokemons);
+  */
+ /* console.log(arrDataPokemons); */
+ arrDataPokemons.sort(function(a,b) {
+   var x = a.name.toLowerCase();
+   var y = b.name.toLowerCase();
+   return x < y ? -1 : x > y ? 1 : 0;
+ });
+pintarPokemons(arrDataPokemons);
+})
+
+
+const ordenarZa = document.getElementById("ordenarZa");
+ordenarZa.addEventListener("click", () => {
+ /* console.log(ordenarZa); */
+ console.log(arrDataPokemons);
+ arrDataPokemons.sort(function(a,b) {
+   var x = a.name.toLowerCase();
+   var y = b.name.toLowerCase();
+   return x > y ? -1 : x < y ? 1 : 0;
+ });
+pintarPokemons(arrDataPokemons);
+})
+const calculate = document.getElementById("calculate");
+calculate.addEventListener("click", () =>{
+/* console.log(calculate); */
+
+})
+
+const opcionesDeFiltrado = document.getElementById("opcionesDeFiltrado");
+opcionesDeFiltrado.addEventListener("change",() => {
+/* console.log(opcionesDeFiltrado.value); */
+
+})
 
