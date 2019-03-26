7a5a7ff0299362b18dd271ea70c3b23ab65dfcf5
ElviaVega <elvita100@hotmail.com>
2019-03-25 19:53:57 -0500

    Funcion de cambiar kg por lb

--- src/data.js
+++ src/data.js
@@ -1,5 +1,15 @@
 /* /* Manejo de data */
-window.example = example;
-const example = () => {
+//window.example = example;
+/*const example = () => {
   return 'example';
+};*/
+const converterLb = (arrPokemons) => {
+  let arrPokemonLb = arrPokemons
+  for(let i=0; i<arrPokemonLb.length; i++){
+    const weightLibras=Math.round((parseFloat(arrPokemonLb[i].weight)*2.20462)*100)/100 ;
+    const resultado = weightLibras +  " lb";
+    arrPokemonLb[i].weightLibras = resultado;
+  };
+  return arrPokemonLb;
 };
+window.converterLb = converterLb;
