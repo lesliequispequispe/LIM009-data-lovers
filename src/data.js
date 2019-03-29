064a3a4be541f9dba3df97dfe9b42c68128a1cbb
ElviaVega <elvita100@hotmail.com>
2019-03-29 10:48:36 -0500

    organizando las funciones

--- src/data.js
+++ src/data.js
@@ -3,13 +3,42 @@
 /*const example = () => {
   return 'example';
 };*/
-const converterLb = (arrPokemons) => {
-  let arrPokemonLb = arrPokemons
-  for(let i=0; i<arrPokemonLb.length; i++){
-    const weightLibras=Math.round((parseFloat(arrPokemonLb[i].weight)*2.20462)*100)/100 ;
-    const resultado = weightLibras +  " lb";
-    arrPokemonLb[i].weightLibras = resultado;
-  };
-  return arrPokemonLb;
-};
-window.converterLb = converterLb;
+const filtrandoPokemonsTipo = (data,condicion) => {
+  const filtrandoPokemons = data.filter(
+   (a) => 
+   a.type[0] === condicion || a.type[1]===condicion);
+return filtrandoPokemons;
+}
+window.filtrandoPokemonsTipo = filtrandoPokemonsTipo;
+
+const ordenandoPokemonesAscendente = (data,sortBy,sortOrder) => {
+data.sort(function(a,b) {
+ sortBy==name;
+ var x = a.name.toLowerCase();
+ var y = b.name.toLowerCase();
+ return x < y ? -1 : x > y ? 1 : 0;
+ 
+});
+return data;
+}
+window.ordenandoPokemonesAscendente=ordenandoPokemonesAscendente;
+
+const calculandoPesoKgLb = (data)=>{
+let arrPokemonLb = data;
+
+for(let i = 0; i<arrPokemonLb.length;i++){
+    const PesoLibras = Math.round((parseFloat(arrPokemonLb[i].weight)*2.20462)*100)/100;
+    const resultado = PesoLibras + " lb";
+arrPokemonLb[i].weightLibras = resultado;
+}
+return arrPokemonLb;
+}
+window.calculandoPesoKgLb=calculandoPesoKgLb;
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -7,64 +7,50 @@ const pintarPokemons = (data) => {
  let string = '';
  for(let i = 0; i < data.length; i++){
    string += `<div class="ancho">
-               <img src=${data[i].img} alt=${data[i].name} />
-               <div>
-                <h3>nombre: ${data[i].name} </h3>
-                <p>altura: ${data[i].height}</p> 
-                <p>peso:${data[i].weight}</p>
-                <p>posibilidad de engendro:${data[i].spawn_chance}</p>
-                <p>huevo:${data[i].egg}</p> 
-                <p>avg engendra:${data[i].avg_spawns}</p>
-                <p>tipo:${data[i].type}</p>
-                <p>debilidades:${data[i].weaknesses}</p>
-                <p>multiplicadores:${data[i].multipliers}</p>  
-              </div>
-              </div>`
+                <img src=${data[i].img} alt=${data[i].name} />
+                  <div>
+                    <h3>nombre: ${data[i].name} </h3>
+                    <p>altura: ${data[i].height}</p> 
+                    <p>peso:${data[i].weight}</p>
+                    <p>posibilidad de engendro:${data[i].spawn_chance}</p>
+                    <p>huevo:${data[i].egg}</p> 
+                    <p>avg engendra:${data[i].avg_spawns}</p>
+                    <p>tipo:${data[i].type}</p>
+                    <p>debilidades:${data[i].weaknesses}</p>
+                    <p>multiplicadores:${data[i].multipliers}</p>
+                    <p>peso libras:${data[i].weightLibras}</p>  
+                  </div>
+              </div>`;
  }
- containerPokemonsElement.innerHTML = string;
+ return containerPokemonsElement.innerHTML = string;
 };
 /* llamando a la función para pintar los pokemons */
 pintarPokemons(arrDataPokemons);
-/* evento click para nuestros botones */
-
-const valoresSelect = document.getElementById("valores-select")
 
 const ordenarAz = document.getElementById("ordenarAz");
 ordenarAz.addEventListener("click", () => {
- /*  console.log(ordenarAz); */
-/*  const arrayValuesDePokemons = Object.values(POKEMON);
- console.log(arrayValuesDePokemons);
-  */
- /* console.log(arrDataPokemons); */
- arrDataPokemons.sort(function(a,b) {
-   var x = a.name.toLowerCase();
-   var y = b.name.toLowerCase();
-   return x < y ? -1 : x > y ? 1 : 0;
- });
-pintarPokemons(arrDataPokemons);
+pintarPokemons(ordenandoPokemonesAscendente(arrDataPokemons,name,ordenarAz));
 })
-
 
 const ordenarZa = document.getElementById("ordenarZa");
 ordenarZa.addEventListener("click", () => {
- /* console.log(ordenarZa); */
- console.log(arrDataPokemons);
- arrDataPokemons.sort(function(a,b) {
-   var x = a.name.toLowerCase();
-   var y = b.name.toLowerCase();
-   return x > y ? -1 : x < y ? 1 : 0;
- });
-pintarPokemons(arrDataPokemons);
+pintarPokemons(ordenandoPokemonesAscendente(arrDataPokemons,name,ordenarZa).reverse());
 })
-const calculate = document.getElementById("calculate");
-calculate.addEventListener("click", () =>{
-/* console.log(calculate); */
 
-})
+const calculate = document.getElementById("calculate");
+calculate.addEventListener("click", () => {
+pintarPokemons(calculandoPesoKgLb(arrDataPokemons));
+ })
 
 const opcionesDeFiltrado = document.getElementById("opcionesDeFiltrado");
 opcionesDeFiltrado.addEventListener("change",() => {
-/* console.log(opcionesDeFiltrado.value); */
+let opcionesFiltradoValue =  opcionesDeFiltrado.value;
+pintarPokemons(filtrandoPokemonsTipo(arrDataPokemons,opcionesFiltradoValue));
+ })
+ const tipo = arrDataPokemons[0].type
+console.log(tipo);
+ //arrType=[]
+ //for (let i=0; arrDataPokemons.length; i++){
 
-})
-
+ //}
+ 
