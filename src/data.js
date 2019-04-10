048ae2850f01d8bc616346aa7c931995ad5f4a7c
ElviaVega <elvita100@hotmail.com>
2019-04-10 08:46:07 -0500

    pasasndo test

--- src/data.js
+++ src/data.js
@@ -1,40 +1,30 @@
-
-const filtrandoPokemonsTipo = (data,condicion) => {
+const filtrandoPokemonsTipo = (data, condicion) => {
   const filtrandoPokemons = data.filter(
-   (a) => 
-   a.type[0] === condicion || a.type[1]===condicion);
-return filtrandoPokemons;
-}
+    (filtro) =>
+      filtro.type[0] === condicion || filtro.type[1] === condicion);
+  return filtrandoPokemons;
+};
 window.filtrandoPokemonsTipo = filtrandoPokemonsTipo;
 
-const ordenandoPokemonesAscendente = (data,sortBy) => {
-data.sort(function(a,b) {
- sortBy==name;
- var x = a.name.toLowerCase();
- var y = b.name.toLowerCase();
- return x < y ? -1 : x > y ? 1 : 0;
- 
-});
-return data;
-}
-window.ordenandoPokemonesAscendente=ordenandoPokemonesAscendente;
+const ordenandoPokemonesAscendente = (data, sortBy) => {
+  data.sort(function(aa, bb) {
+    sortBy === name;
+    let xx = aa.name.toLowerCase();
+    let yy = bb.name.toLowerCase();
+    return xx < yy ? -1 : xx > yy ? 1 : 0;
+  });
+  return data;
+};
 
-const calculandoPesoKgLb = (data)=>{
-let arrPokemonLb = data;
+window.ordenandoPokemonesAscendente = ordenandoPokemonesAscendente;
 
-for(let i = 0; i<arrPokemonLb.length;i++){
-    const PesoLibras = Math.round((parseFloat(arrPokemonLb[i].weight)*2.20462)*100)/100;
-    const resultado = PesoLibras + " lb";
-arrPokemonLb[i].weightLibras = resultado;
-}
-return arrPokemonLb;
-}
-window.calculandoPesoKgLb=calculandoPesoKgLb;
-
-
-
-
-
-
-
-
+const calculandoPesoKgLb = (data) => {
+  let arrPokemonLb = data;
+  for (let i = 0; i < arrPokemonLb.length; i++) {
+    const PesoLibras = Math.round((parseFloat(arrPokemonLb[i].weight) * 2.20462) * 100) / 100;
+    const resultado = PesoLibras + ' lb';
+    arrPokemonLb[i].weightLibras = resultado;
+  }
+  return arrPokemonLb;
+};
+window.calculandoPesoKgLb = calculandoPesoKgLb;
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,16 +1,204 @@
 global.window = global;
-global.assert = require('chai').assert;
 require('../src/data');
-require('./data.spec.js');
+
+const dataInput = [
+  { "id": 1,
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "6.9 kg"},
+  {"id": 2,
+    "name": "Ivysaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "13.0 kg"},
+  {"id": 3,
+    "name": "Venusaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "100.0 kg"},
+  {"id": 4,
+    "name": "Charmander",
+    "type": [
+      "Fire"
+    ],
+    "weight": "8.5 kg" },
+  {"id": 151,
+    "name": "Mew",
+    "type": [
+      "Psychic"
+    ],
+    "weight": "4.0 kg"}
+];
+
+const dataInput2 = [
+  { "id": 1,
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "6.9 kg"},
+  {"id": 2,
+    "name": "Ivysaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "13.0 kg"},
+  {"id": 3,
+    "name": "Venusaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "100.0 kg"},
+  {"id": 4,
+    "name": "Charmander",
+    "type": [
+      "Fire"
+    ],
+    "weight": "8.5 kg" },
+  {"id": 151,
+    "name": "Mew",
+    "type": [
+      "Psychic"
+    ],
+    "weight": "4.0 kg"}
+];
 
 
-describe('example', () => {
-  
-  it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+const dataOuput = [
+  { "id": 1,
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "6.9 kg"},
+  { "id": 4,
+    "name": "Charmander",
+    "type": [
+      "Fire"
+    ],
+    "weight": "8.5 kg"},
+  {"id": 2,
+    "name": "Ivysaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "13.0 kg"}, 
+  { "id": 151,
+    "name": "Mew",
+    "type": [
+      "Psychic"
+    ],
+    "weight": "4.0 kg"},
+  {"id": 3,
+    "name": "Venusaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "100.0 kg"} 
+];
+
+const dataOuput1 = [
+  { "id": 1,
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "6.9 kg"},
+  {"id": 2,
+    "name": "Ivysaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "13.0 kg"},
+  {"id": 3,
+    "name": "Venusaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "100.0 kg"},
+];
+const dataOuput2 = [
+  { "id": 1,
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "6.9 kg",
+    "weightLibras": "15.21 lb" },
+  {"id": 2,
+    "name": "Ivysaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "13.0 kg",
+    "weightLibras": "28.66 lb"},
+  {"id": 3,
+    "name": "Venusaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "weight": "100.0 kg",
+    "weightLibras": "220.46 lb"},
+  {"id": 4,
+    "name": "Charmander",
+    "type": [
+      "Fire"
+    ],
+    "weight": "8.5 kg",
+    "weightLibras": "18.74 lb" },
+  {"id": 151,
+    "name": "Mew",
+    "type": [
+      "Psychic"
+    ],
+    "weight": "4.0 kg",
+    "weightLibras": "8.82 lb"}
+];
+
+const sortby = "name";
+const condicion = "Grass";
+
+
+describe('OrdenandoPokemonesAscendente', () => {
+  it('Es una funcion', () => {
+    expect(typeof ordenandoPokemonesAscendente).toBe('function');
   });
-
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+  it('array ordenado', () => {
+    expect(ordenandoPokemonesAscendente(dataInput, sortby)).toEqual(dataOuput);
   });
-})
+});
+describe('filtrandoPokemonsTipo', () => {
+  it('Es una funcion', () => {
+    expect(typeof filtrandoPokemonsTipo).toBe('function');
+  });
+  it('array filtrado', () => {
+    expect(filtrandoPokemonsTipo(dataInput, condicion)).toEqual(dataOuput1);
+  });
+});
+describe('calculandoPesoKgLb', () => {
+  it('Es una funcion', () => {
+    expect(typeof calculandoPesoKgLb).toBe('function');
+  });
+  it('array con la propiedad peso en libras', () => {
+    expect(calculandoPesoKgLb(dataInput2)).toEqual(dataOuput2);
+  });
+});
